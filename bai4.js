01aeac1689b826430e6d1e5ae496d71d8387c84f
dungbeo171 <dungtc17012005@gmail.com>
2025-07-08 18:19:36 +0700

    Delete bai4.js

--- bai4.js
+++
@@ -1,5 +0,0 @@
-function countOccurrences(arr, value) {
-    const a = arr.reduce((total, current) => current == value ? total+=1 : total, 0);
-    return a;
-}
-console.log(countOccurrences([1, 2, 3, 2, 1, 2], 2));
