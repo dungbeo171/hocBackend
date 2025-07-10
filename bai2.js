e3f15700f6f1b9aa42e2f82e14d5833bcdabea8c
dungbeo171 <dungtc17012005@gmail.com>
2025-07-10 15:04:35 +0700

    Delete bai2.js

--- bai2.js
+++
@@ -1,7 +0,0 @@
-function findMax(arr) {
-    const find_max = arr.reduce(function(max, current){
-        return current > max ? current : max;
-    },0);
-    return find_max;
-}
-console.log(findMax([10, 20, 30, 5])); 
